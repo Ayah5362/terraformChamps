6bcbadb0eb39cf74db1d6e5ab1b55b6e905f069b
Ayah5362 <102102296+Ayah5362@users.noreply.github.com>
2024-06-22 17:59:51 +0300

    Create provider.tf

---
+++ Day four/provider.tf
@@ -0,0 +1,16 @@
+# Configure aws provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "erakiterrafromstatefiles"
+    key     = "aya-state-file.tfstate"
+    region  = "us-east-1"
+    profile = "eraki"
+  }
+}
